2c733e58dad8b5b76814b37827f2f0b1eca3ee5f
yuji masujima <masjima2712@gmail.com>
2019-12-27 07:50:05 +0900

    Update Controller for viewing foodsdata

--- src/main/java/com/PFCbalancer/controller/MainPageController.java
+++ src/main/java/com/PFCbalancer/controller/MainPageController.java
@@ -16,7 +16,7 @@ import com.PFCbalancer.model.PFC;
 
 @Controller
 public class MainPageController {
-	
+
     String label[] = {"Carbohydrate(炭水化物)","Fat(脂質)","Protein(タンパク質)"};
 
     int kal;
@@ -24,82 +24,82 @@ public class MainPageController {
     int fat;
     int carb;
     int pfcData[] = {protein, fat, carb};
-    
-	String foodsName;
-	int foodsProtein;
-	int foodsFat;
-	int foodsCarb;
-    
-    
+
+//		String foodsName;
+		int foodsProtein;
+		int foodsFat;
+		int foodsCarb;
+
+
 
 	@GetMapping("/")
 	public String getMain(@ModelAttribute FormBodyData formBodyData, FormFoodsData formFoodsData, Model model) {
-		
+
 		model.addAttribute("label",label);
-        model.addAttribute("protein",protein);
-        model.addAttribute("fat",fat);
-        model.addAttribute("carb",carb);
-        model.addAttribute("kal",kal); 
-		
+    model.addAttribute("protein",protein);
+    model.addAttribute("fat",fat);
+    model.addAttribute("carb",carb);
+    model.addAttribute("kal",kal);
+
 //		model.addAttribute("foodsName",foodsName);
-//        model.addAttribute("foodsProtein",foodsProtein);
-//        model.addAttribute("foodsFat",foodsFat);
-//        model.addAttribute("foodsCarb",foodsCarb);
-		
-//        model.addAttribute("formFoodsData",formFoodsData); 
-		
+    model.addAttribute("foodsProtein",foodsProtein);
+    model.addAttribute("foodsFat",foodsFat);
+    model.addAttribute("foodsCarb",foodsCarb);
+
+//        model.addAttribute("formFoodsData",formFoodsData);
+
 		return "mainPage";
 	}
-	
+
 	@PostMapping("/main/calcIdealPFC")
 	public String postMainIdealPFC(@ModelAttribute FormBodyData formBodyData, FormFoodsData formFoodsData, BindingResult bindingResult, Model model) {
 
 		PFC pfc = new PFC();
-		
+
 		pfc.setWeight(formBodyData.getWeight());
 		pfc.setHeight(formBodyData.getHeight());
 		pfc.setAge(formBodyData.getAge());
 		pfc.setPFC();
-		
+
 		protein = pfc.getProtein();
 		fat = pfc.getFat();
 		carb = pfc.getCarb();
 		kal = protein + fat + carb;
-		
-		model.addAttribute("label",label);
-        model.addAttribute("protein",protein);
-        model.addAttribute("fat",fat);
-        model.addAttribute("carb",carb);
-        model.addAttribute("kal",kal); 
-        
-//        model.addAttribute("foodsName",foodsName);
-//        model.addAttribute("foodsProtein",foodsProtein);
-//        model.addAttribute("foodsFat",foodsFat);
-//        model.addAttribute("foodsCarb",foodsCarb);
-        		
-		return "mainPage";
-	}
-	
-	@PostMapping("/main/calcNowPFC")
-	public String postMainNowPFC(@ModelAttribute FormBodyData formBodyData, FormFoodsData formFoodsData, BindingResult bindingResult, Model model) {
-		
-//		model.addAttribute("formFoodsData",formFoodsData);
-		
-		List<FormFoodsData> foodsList = new ArrayList<>();
-		foodsList.add(formFoodsData);
-		model.addAttribute("foodsList",foodsList);
-		
+
 		model.addAttribute("label",label);
         model.addAttribute("protein",protein);
         model.addAttribute("fat",fat);
         model.addAttribute("carb",carb);
         model.addAttribute("kal",kal);
-        
+
 //        model.addAttribute("foodsName",foodsName);
 //        model.addAttribute("foodsProtein",foodsProtein);
 //        model.addAttribute("foodsFat",foodsFat);
 //        model.addAttribute("foodsCarb",foodsCarb);
-		
+
+		return "mainPage";
+	}
+
+	@PostMapping("/main/calcNowPFC")
+	public String postMainNowPFC(@ModelAttribute FormBodyData formBodyData, FormFoodsData formFoodsData, BindingResult bindingResult, Model model) {
+
+//		model.addAttribute("formFoodsData",formFoodsData);
+
+		List<FormFoodsData> foodsList = new ArrayList<>();
+		foodsList.add(formFoodsData);
+		model.addAttribute("foodsList",foodsList);
+
+		model.addAttribute("label",label);
+        model.addAttribute("protein",protein);
+        model.addAttribute("fat",fat);
+        model.addAttribute("carb",carb);
+        model.addAttribute("kal",kal);
+
+//        model.addAttribute("foodsName",foodsName);
+//        model.addAttribute("foodsProtein",foodsProtein);
+//        model.addAttribute("foodsFat",foodsFat);
+//        model.addAttribute("foodsCarb",foodsCarb);
+
 		return "mainPage";
 	}
 }
